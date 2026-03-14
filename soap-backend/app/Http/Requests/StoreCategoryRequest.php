5b3b5a90f097d0a7bf1a540e3652e2233ed2f0c7
Romeoo963 <rame200226@gmail.com>
2026-03-14 17:02:17 +0300

    add category service/request

--- soap-backend/app/Http/Requests/StoreCategoryRequest.php
+++ soap-backend/app/Http/Requests/StoreCategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCategoryRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,12 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('categories', 'name')
+            ]
         ];
     }
 }
